d56e6a4fe419bd2e8b4541d66a352daf93233b69
epampam <epampam@wp.pl>
2023-02-18 17:12:50 +0100

    all task finished

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatest = (first > second && first > third) ? first :
+                (second > first && second > third) ? second :
+                        third;
+        System.out.println(greatest);
+    }
+    public static void main(String[] args) {
+        MaxNumberPrinter maxNumberPrinter = new MaxNumberPrinter();
+        maxNumberPrinter.printGreatest(2,2,1);
     }
 }
+
